06ca8e9a963b3a064d93c2a1751ff1b4631ef2e0
Hlaing Htet <99160494+Hlaing-Htet@users.noreply.github.com>
2023-01-16 11:22:33 +0630

    done

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { IoLogoTwitter } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <a href="#" className="footer__logo">
+        HH
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experence">Experence</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <IoLogoTwitter />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; HH . All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
